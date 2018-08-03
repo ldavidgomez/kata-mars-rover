ff9c778294c54b8e57c90931384ce0e807bd183a
David Gomez <david.gomez@apiumhub.com>
2018-08-03 15:59:27 +0200

    fixed wrong command

--- src/test/kotlin/RoverSpec.kt
+++ src/test/kotlin/RoverSpec.kt
@@ -5,26 +5,6 @@ import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
 import kotlin.test.assertFailsWith
 
-/*
-You’re part of the team that explores Mars by sending remotely controlled vehicles to the surface of the planet.
-Develop an API that translates the commands sent from earth to instructions that are understood by the rover.
-
-Requirements
-You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W) it is facing.
-The rover receives a character array of commands.
-Implement commands that move the rover forward/backward (f,b).
-Implement commands that turn the rover left/right (l,r).
-Implement wrapping from one edge of the grid to another. (planets are spheres after all)
-Implement obstacle detection before each move to a new square. If a given sequence of commands encounters an obstacle,
-the rover moves up to the last possible point, aborts the sequence and reports the obstacle.
-
-Rules
-Hardcore TDD. No Excuses!
-Change roles (driver, navigator) after each TDD cycle.
-No red phases while refactoring.
-Be careful about edge cases and exceptions. We can not afford to lose a mars rover, just because the developers overlooked a null pointer.
-*/
-
 class RoverSpek: Spek({
     describe("given a command ") {
 
@@ -37,6 +17,7 @@ class RoverSpek: Spek({
                 roverPosition `should equal` Pair(0,1)
             }
         }
+
         on("forward command and South direction") {
 
             it("should rover moves backwards") {
@@ -88,13 +69,31 @@ class RoverSpek: Spek({
             }
         }
 
+        on("wrong command") {
+
+            it("should throw exception") {
+                val command = arrayOf("wrong command")
+                assertFailsWith<IncorrectCommandException> {
+                    val rover = Rover(0, 0, "N")
+                    rover.move(command)
+                }
+            }
+        }
+
     }
 })
 
 class IncorrectPositionException : Exception()
+class IncorrectCommandException : Exception()
+
+enum class COMMANDS {
+    F, B, E, W
+}
 
 class Rover(private val x: Int, private val y: Int, private val direction: String) {
     fun move(commands: Array<String>): Pair<Int, Int> {
+        val validCommands = try { commands.map { COMMANDS.valueOf(it.toUpperCase()) } } catch (e: Exception) {throw IncorrectCommandException()}
+
         val moveMap = mapOf("N" to Pair(0,1), "S" to Pair(0, -1) )
 
         val addedPosition = when(moveMap.containsKey(direction)) {
@@ -105,9 +104,9 @@ class Rover(private val x: Int, private val y: Int, private val direction: Strin
         if (commands.isEmpty()) {
             return Pair(x, y)
         }
-        return when (commands[0]) {
-            "f" -> Pair(x + addedPosition.first, y + addedPosition.second)
-            "b" -> Pair(x + addedPosition.first, y - addedPosition.second)
+        return when (validCommands[0]) {
+            COMMANDS.F -> Pair(x + addedPosition.first, y + addedPosition.second)
+            COMMANDS.B -> Pair(x + addedPosition.first, y - addedPosition.second)
             else -> throw Exception()
         }
     }
