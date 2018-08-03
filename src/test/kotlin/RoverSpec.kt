baf7084326974a705eaeac98f9c89480d053f030
David Gomez <david.gomez@apiumhub.com>
2018-08-03 19:28:17 +0200

    added turn right test

--- src/test/kotlin/RoverSpec.kt
+++ src/test/kotlin/RoverSpec.kt
@@ -149,6 +149,16 @@ class RoverSpek: Spek({
                 roverPosition `should equal` Position(0,0, "W")
             }
         }
+
+        on("right command and South direction") {
+
+            it("should rover turn right and direction is West") {
+                val command = arrayOf("r")
+                val rover = Rover(Position(0, 0, "W"))
+                val roverPosition = rover.move(command)
+                roverPosition `should equal` Position(0,0, "N")
+            }
+        }
     }
 })
 
